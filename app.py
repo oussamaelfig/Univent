6050c30a69e95cc9bd0083cbb6eac31f722b53a7
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2023-06-28 23:24:32 -0400

    debut validation server-side

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sqlite3
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, session
 from werkzeug.utils import secure_filename
 from database import Database
+import re
 
 app = Flask(__name__)
 
@@ -65,5 +66,53 @@ def create_event():
     return render_template("create_event.html")
 
 
+@app.route("/create_account", methods=["GET", "POST"])
+def creer_compte():
+    if request.method == "POST":
+        nom = request.form["nom"]
+        courriel = request.form["courriel"]
+        mdp = request.form["mdp"]
+        mdp_conf = request.form["mdpConf"]
+        type_compte = request["type"]
+        err = valider_compte(nom, courriel, type_compte)
+        err.insert(valider_mdp(mdp, mdp_conf))
+        if len(err) != 0:
+            return render_template("create_account.html", erreurs=err) 
+        # creer dans bd.
+    return render_template("create_account.html")
+    
+
+def valider_compte(nom, courriel, type_compte):
+    err = []
+    regex = r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
+    if nom == "" or len(nom) < 100:
+        err.insert("Le nom entré est invalide (100 caractères maximum).")
+    if courriel == "" or len(courriel) < 100 or not re.fullmatch(regex, courriel): #REGEX EST COURRIEL.
+        err.insert("Le courriel entré est invalide.")
+    return err
+
+def valider_mdp(mdp, mdp2):
+    err = []
+    a_err = False
+    punctuation = ['!', ',', '?', '.', '¡', ';', '¿']
+    if mdp == "" or len(mdp):
+        err.insert("Le mot de passe entré est invalide.")
+        a_err = True
+    if re.search('[0-9]', mdp) is None and not a_err:
+        err.insert("Le mot de passe entré est invalide.")
+        a_err = True
+    if re.search('[A-Z]', mdp) is None and not a_err:
+        err.insert("Le mot de passe entré est invalide.")
+        a_err = True
+    for c in mdp:
+        if c in punctuation and not a_err:
+            return err
+    err.insert("Le mot de passe entré est invalide.")    
+    if mdp != mdp2:
+        err.insert("Les deux mots de passe ne concordent pas.")
+    return err
+
 if __name__ == "__main__":
     app.run()
+
+app.secret_key = "qweiqen1@!@#SADASFA@#!!!+_$"
