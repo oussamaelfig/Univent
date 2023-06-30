13ba69b3c01f8d4a69ce14ebfd5ad943e740b3a9
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2023-06-30 15:36:38 -0400

    close #14 #15

--- database.py
+++ database.py
@@ -5,13 +5,38 @@ class Database:
     
     def get_connexion(self):
         if self.connection is None:
-            self.connection = sqlite3.connect("database.db")
+            self.connection = sqlite3.connect("db/database.db")
         return self.connection
         
     def close_connection(self):
         if self.connection is not None:
             self.connection.close()
             
+    #### Table users #####
+    def creer_user(self, identifiant, nom, courriel, type_c, hache, salt):
+        connection = self.get_connexion()
+        cursor = connection.cursor()
+        cursor.execute("insert into users(identifiant, nom, typeCompte, courriel, hache, salt)"
+                       "values(?,?,?,?,?,?)", 
+                       (identifiant, nom, type_c, courriel, hache, salt,))
+        connection.commit()
+            
+    def get_user_from_iden(self, ident):
+        cursor = self.get_connexion().cursor()
+        cursor.execute("select identifiant from users where identifiant = ?", (ident, ))
+        return cursor.fetchone()
+    
+    def get_user_from_courriel(self, courriel):
+        cursor = self.get_connexion().cursor()
+        cursor.execute("select identifiant from users where courriel = ?", (courriel, ))
+        return cursor.fetchone()
+    
+    def get_user_pass_from_courriel(self, courriel):
+        cursor = self.get_connexion().cursor()
+        cursor.execute("select hache, salt from users where courriel = ?", (courriel, ))
+        return cursor.fetchone()
+    
+    #### Table events #####
     def creer_new_evenement(self, creator_id, title, start_date_time, end_date_time,
                             location, flyer_image_name, description):
         connect = self.get_connexion()
@@ -32,3 +57,5 @@ class Database:
             ),
         )
         connect.commit()
+        
+    
