f7bc0cc3a47e0a07963f5cd7adb565bbd95afec7
EL-FIGHA <oelfigha2002@gmail.com>
2023-07-10 12:20:17 -0400

    close 18

--- db/script.sql
+++ db/script.sql
@@ -1,18 +1,19 @@
 create table users
 (
-    id         integer primary key,
+    id          integer primary key,
     identifiant varchar(32),
-    nom        varchar(100),
-    typeCompte integer,
-    courriel   varchar(100),
-    hache      varchar(32),
-    salt       varchar(128)
+    nom         varchar(100),
+    typeCompte  integer,
+    courriel    varchar(100),
+    hache       varchar(32),
+    salt        varchar(128)
 );
 
-create table sessions(
-    id integer PRIMARY KEY,
+create table sessions
+(
+    id          integer PRIMARY KEY,
     identifiant varchar(32),
-    userId varchar(32)
+    userId      varchar(32)
 );
 
 CREATE TABLE Events
@@ -27,5 +28,14 @@ CREATE TABLE Events
     description      TEXT,
     max_registration INTEGER,
     FOREIGN KEY (creator_id) REFERENCES users (id)
+);
+
+CREATE TABLE Participants
+(
+    id       INTEGER PRIMARY KEY,
+    event_id INTEGER,
+    nom      TEXT NOT NULL,
+    email    TEXT NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES Events (event_id)
 );
 
